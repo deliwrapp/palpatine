0535fc4996fd6b51dae884c051b60ec7da6b8c6a
deliwrapp <100473820+deliwrapp@users.noreply.github.com>
2022-03-21 11:12:42 +0100

    Revert "Update Template and add dashboard admin"

---
+++ src/Post/Form/PostFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Post\Form;
+
+use App\Post\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class PostFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('author');
+            
+        if ($options) {
+            $builder
+                ->add('submit', SubmitType::class, [
+                    'label' => $options['change'],
+                ]);
+        } else {
+            $builder
+                ->add('submit', SubmitType::class, [
+                    'label' => 'Validate'
+                ]);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+            'change' => 'Validate',
+            // enable/disable CSRF protection for this form
+            'csrf_protection' => true,
+            // the name of the hidden HTML field that stores the token
+            'csrf_field_name' => '_token',
+            // an arbitrary string used to generate the value of the token
+            // using a different string for each form improves its security
+            'csrf_token_id'   => 'post_item',
+        ]);
+    }
+}
